3b63804d0772b0b412db2ced24669f656e219e00
leepete <peterly@hotmail.co.nz>
2018-09-11 13:41:33 +1200

    Initial Commit

---
+++ GO-Native/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GO-Native
+//
+//  Created by Peter Lee on 11/09/18.
+//  Copyright © 2018 Peter Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
